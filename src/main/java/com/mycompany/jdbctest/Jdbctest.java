a485e48403ab99ce061b8c71e843606dc97cf58c
David Sanchez <davidendubai@gmail.com>
2024-08-24 18:40:38 -0500

    Se crea usuario, password y url para conectar base de datos

---
+++ src/main/java/com/mycompany/jdbctest/Jdbctest.java
@@ -0,0 +1,9 @@
+package com.mycompany.jdbctest;
+public class Jdbctest {
+    public static void main(String[] args) {
+        String usuario="root";
+        String password="";
+        String url="jdbc:mysql://localhost:3306/Jdbctest";
+        
+    }
+}
