901a7f2777a367b520da0eea84bbd54fbf73e6f0
David Sanchez <davidendubai@gmail.com>
2024-08-24 18:58:01 -0500

    Se importan dependencias y se crea try and catch

--- src/main/java/com/mycompany/jdbctest/Jdbctest.java
+++ src/main/java/com/mycompany/jdbctest/Jdbctest.java
@@ -1,9 +1,28 @@
 package com.mycompany.jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Jdbctest {
     public static void main(String[] args) {
         String usuario="root";
         String password="";
         String url="jdbc:mysql://localhost:3306/Jdbctest";
+        Connection conexion;
+        Statement statement;
+        ResultSet rs;
+        
+        try {
+            conexion = DriverManager.getConnection(url, usuario, password);
+                    
+                    } catch (SQLException ex) {
+            Logger.getLogger(Jdbctest.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
 }
