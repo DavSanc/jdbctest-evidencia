6a77ad307e76ed694c605263ae4d012740d1b192
David Sanchez <davidendubai@gmail.com>
2024-08-25 05:24:36 -0500

    Se inserta usuario acardona

--- src/main/java/com/mycompany/jdbctest/Jdbctest.java
+++ src/main/java/com/mycompany/jdbctest/Jdbctest.java
@@ -1,6 +1,7 @@
 package com.mycompany.jdbctest;
 
-import java.sql.Connection;
+
+import java.sql.Connection; // se importa clase llamada .Conection
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,17 +11,21 @@ import java.util.logging.Logger;
 
 public class Jdbctest {
     public static void main(String[] args) {
-        String usuario="root";
-        String password="";
-        String url="jdbc:mysql://localhost:3306/Jdbctest";
-        Connection conexion;
+        String usuario="root"; //Crea variable usuario y contiene dentro, texto root
+        String password=""; // Variable vacia
+        String url="jdbc:mysql://localhost:3306/Jdbctest"; //Url de la base de datos
+        Connection conexion; //Crear constante para almacenar conexion
         Statement statement;
         ResultSet rs;
         
         try {
-            conexion = DriverManager.getConnection(url, usuario, password);
-                    
-                    } catch (SQLException ex) {
+            conexion = DriverManager.getConnection(url, usuario, password); 
+            statement = conexion.createStatement();
+            rs = statement.executeQuery("SELECT * FROM usuarios");
+            while(rs.next()) {
+                System.out.println(rs.getString("nombre"));
+            }
+        } catch (SQLException ex) {
             Logger.getLogger(Jdbctest.class.getName()).log(Level.SEVERE, null, ex);
         }
         
